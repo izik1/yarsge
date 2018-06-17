626e9ca75983e5ef4a5dfcec61201ce690f45c79
Zachery Gyurkovitz <zgyurkovitz@gmail.com>
2018-06-16 19:43:53 -0700

    Update `rust-toolchain` :arrow_up:

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // Copyright Zachery Gyurkovitz 2017-2018 MIT License, see licence.md for more details.
 
-#![feature(nll, termination_trait)]
+#![feature(nll)]
 #![cfg_attr(feature = "cargo-clippy", allow(verbose_bit_mask))]
 
 extern crate rgb;
